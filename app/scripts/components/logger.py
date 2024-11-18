141c88d2c1836de436a9d7db532d4348106d680f
Python arch <py_official@mail.ru>
2024-11-18 21:54:16 +0300

    Global update for logger

--- app/scripts/components/logger.py
+++ app/scripts/components/logger.py
@@ -1,6 +1,3 @@
-"""
-Logger 0.1a
-"""
 from datetime import datetime
 from app.scripts.components.jsonmanager import JsonManager, AddressType
 from sys import stdout
@@ -8,16 +5,8 @@ from typing import TextIO
 from colorama import init, Fore, Style
 init()
 
-"""
-Usage:
-printf(text_print, suffix_id{0: info, 1:warn, 2:error}, log_text_in_file)
-Example:
-from logger import Logger, TypeLogText
-l = Logger('TestModule')
 
-l.printf('hello world!')
-l.printf('some annoying and popular warn', type_message=TypeLogText., log_text_in_file=False)
-"""
+VERSION = 2
 
 
 class LogType:
@@ -31,28 +20,43 @@ class LogType:
     FATAL = 4
 
 
+class Colors:
+    """
+    Color themes for logger output
+    """
+    time = f"{Style.BRIGHT}[{Fore.CYAN}{{now_time}}{Fore.RESET}] "
+    name = f"[{Fore.GREEN}{{name}}{Fore.RESET}] "
+    log_types = ["INFO ", "DEBUG", "WARN ", "ERROR", "FATAL"]
+    color_log_types = [
+        f"{Style.BRIGHT}[{Fore.CYAN}INFO {Fore.RESET}] ",
+        f"{Style.BRIGHT}[{Fore.GREEN}DEBUG{Fore.RESET}] ",
+        f"{Style.BRIGHT}[{Fore.YELLOW}WARN {Fore.RESET}] ",
+        f"{Style.BRIGHT}[{Fore.RED}ERROR{Fore.RESET}] ",
+        f"{Style.BRIGHT}[{Fore.RED}FATAL{Fore.RESET}] "]
+    color_line = ["{line}", "{line}", "{line}", "{line}", f"{Fore.RED}{{line}}"]
+
+
 # main class of this module
 class Logger:
-    def __init__(self, module_prefix: str, debug_mess: int = None,  out_stream: TextIO = None):
+    def __init__(self, name: str, debug_mess: int = None,  out_stream: TextIO = None):
         # get conf to logger
-        jsm = JsonManager(AddressType.FILE, "logger_conf.json")
-        jsm.load_from_file()
-        self.logger_conf = jsm.get_buffer()
+        self.cfg = JsonManager(AddressType.FILE, "cfg.json")
+        self.cfg.load_from_file()
         self.out_stream = out_stream
         if out_stream is None:
             self.out_stream = stdout.orig_out_stream if isinstance(stdout, PrintHandler) else stdout
         self._debug_mess = debug_mess
-        if debug_mess is None:
-            self.debug_mess = stdout.debug_mess if isinstance(stdout, PrintHandler) else 0
         # init class data, prefix
-        self.module_prefix = module_prefix
+        self.name = name
         self.__old_date = ""
         self.__path_to_log_file = ""
-        self.color_suf = [Fore.CYAN, Fore.GREEN, Fore.YELLOW, Fore.RED, Fore.RED]
-        self.suffixes = ["INFO ", "DEBUG", "WARN ", "ERROR", "FATAL"]
+        self.msg_format = self.cfg["msg_format"] + Fore.RESET
 
     def __str__(self):
-        return self.module_prefix
+        return self.name
+
+    def set_debug_logging(self, enable: bool):
+        self._debug_mess = enable
 
     @staticmethod
     def __get_str_datetime(time, datetime_format: str) -> str:
@@ -63,45 +67,43 @@ class Logger:
         # check if change the date
         if self.__old_date != new_date:
             # create new file
-            self.__path_to_log_file = f"{self.logger_conf['default_path']}{self.module_prefix}_{new_date}.txt"
-            with open(self.__path_to_log_file, "w", encoding=self.logger_conf["encoding"]) as file:
-                file.write(f"Logger version {self.logger_conf['version']} | Log of module --> {self.module_prefix}\n")
+            self.__path_to_log_file = f"{self.cfg['default_path']}{self.name}_{new_date}.txt"
+            with open(self.__path_to_log_file, "w", encoding=self.cfg["encoding"]) as file:
+                file.write(f"Logger version {VERSION} | Log of module --> {self.name}\n")
             self.__old_date = new_date
         # write a note to the file
-        with open(self.__path_to_log_file, "a", encoding=self.logger_conf["encoding"]) as file:
+        with open(self.__path_to_log_file, "a", encoding=self.cfg["encoding"]) as file:
             file.write(line)
 
     # print info
-    def printf(self, line: str, log_type: int = 0, log_text_in_file: bool = True):
-        # generate timestamp, prefix and suffix
+    def printf(self, line: str, log_type: int = 0, end: str = "\n", log_text_in_file: bool = True):
+        # generate timestamp
         now_int_time = datetime.now()
-        now_date = self.__get_str_datetime(now_int_time, self.logger_conf["date_format"])
-        now_time = self.__get_str_datetime(now_int_time, self.logger_conf["time_format"])
-        suffix = self.suffixes[log_type]
+        now_date = self.__get_str_datetime(now_int_time, self.cfg["date_format"])
+        now_time = self.__get_str_datetime(now_int_time, self.cfg["time_format"])
         # generate color text
-        f_line = line
-        if log_type == 4:
-            f_line = Fore.RED + f_line
-        f_line = (f"{Style.BRIGHT}[{Fore.CYAN}{now_time}{Fore.RESET}] " +
-                  f"[{Fore.GREEN}{self.module_prefix}{Fore.RESET}] " +
-                  f"[{self.color_suf[log_type]}{suffix}{Fore.RESET}] " +
-                  f_line)
-
-        print(f_line, file=self.out_stream)
-
+        c_line = self.msg_format.format(now_time=Colors.time.format(now_time=now_time),
+                                        name=Colors.name.format(name=self.name),
+                                        log_type=Colors.color_log_types[log_type],
+                                        line=Colors.color_line[log_type].format(line=line))
+        print(c_line, file=self.out_stream, end=end)
         # need to save note in file
         if log_text_in_file:
             # generate text without ansi color
-            f_line = f"[{now_time}] [{self.module_prefix}] [{suffix}] {line}\n"
+            f_line = self.msg_format.format(now_time=now_time,
+                                            name=self.name,
+                                            log_type=Colors.log_types[log_type],
+                                            line=line
+                                            ) + "\n"
+            # add text to file
             self.__add_note(f_line, now_date)
 
 
 class PrintHandler:
-    def __init__(self, logger: Logger, orig_out_stream: TextIO = stdout, debug_mess: int = 0,
+    def __init__(self, logger: Logger, orig_out_stream: TextIO = stdout,
                  save_to_file: bool = False):
         self.log = logger
         self._orig_out_stream = orig_out_stream
-        self._debug_mess = debug_mess
         self._out_text = ""
         self.save_to_file = save_to_file
 
@@ -109,22 +111,19 @@ class PrintHandler:
     def orig_out_stream(self) -> TextIO:
         return self._orig_out_stream
 
-    @property
-    def debug_mess(self) -> int:
-        return self._debug_mess
-
     def flush(self):
         pass
 
-    def write(self, message: str):
+    def write(self, message: str | bytes):
         if not message:
             return
+        if type(message) is bytes:
+            message = message.decode()
+        self._out_text += message
         if message[-1] == "\n":
-            if self._debug_mess:
-                self.log.printf(self._out_text, LogType.DEBUG, log_text_in_file=self.save_to_file)
+            self.log.printf(self._out_text, LogType.DEBUG, end="", log_text_in_file=self.save_to_file)
             self._out_text = ""
             return
-        self._out_text += message
 
 
 class ErrorHandler:
@@ -137,10 +136,12 @@ class ErrorHandler:
         pass
 
     def write(self, message: str):
-        self._err_text += message
-        if not self._err_text:
+        print(">>>" + message + "<<<")
+        if not message:
             return
-        if self._err_text[-1] == "r":
+        self._err_text += message
+
+        if message[0] == ":":
             self._hand_code = 1
             return
         if self._hand_code and self._err_text[-1] == "\n":
