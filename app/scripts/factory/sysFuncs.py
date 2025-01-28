f43bc597f6467010bc8dccbecbe99d0b54d78aa4
Python arch <py_official@mail.ru>
2025-01-28 17:18:41 +0300

    Del useless check req system

--- app/scripts/factory/sysFuncs.py
+++ app/scripts/factory/sysFuncs.py
@@ -7,12 +7,6 @@ SYM_IDS = ascii_letters + digits
 SYM_LEN = len(SYM_IDS) - 1
 
 
-def check_requirements(req: list):
-    for name, n_ver, h_ver in req:
-        if n_ver != int(h_ver):
-            raise FactoryRequirementVersionError(name, n_ver, h_ver)
-
-
 def generate_id(len_id: int = 8) -> str:
     result = "".join([SYM_IDS[randint(0, SYM_LEN)] for i in range(len_id)])
     return result
