749fd1cf5e6650f2a23d44606c988875b32fa735
Python arch <py_official@mail.ru>
2025-01-28 17:20:20 +0300

    Small fixes

--- app/scripts/main.py
+++ app/scripts/main.py
@@ -8,6 +8,7 @@ sys.path.insert(1, os.path.join(sys.path[0].replace("/app/scripts", "")))
 import bot_manager
 from components.jsonmanager import AddressType, JsonManagerWithCrypt
 
+
 __all__ = [
     "Main"
 ]
@@ -114,6 +115,10 @@ class StartProcedures:
             b = {}
         jsm.set_buffer(b)
         jsm.write_in_file()
+
+    @staticmethod
+    def test():
+        pass
 
 
 class Main:
