7871e81c578e77a9863758e49179488e7aea7e4d
w1ldcard <w1ldcard@unimice.ru>
2026-03-30 19:44:58 +0300

    Add loader

---
+++ app/utils/config/loader.py
@@ -0,0 +1,20 @@
+from pathlib import Path
+
+from json5 import load
+
+from app.factory.settings import ConfigSettings
+from app.utils.config.config import BotConfig
+
+
+def load_config(path: str | Path  = ConfigSettings.CONFIG_PATH) -> BotConfig:
+    if isinstance(path, str):
+        path = Path(path)
+
+    if not path.exists():
+        path.parent.mkdir(parents=True, exist_ok=True)
+
+        with path.open('w') as f:
+            f.write(ConfigSettings.DEFAULT_CONFIG)
+
+    with path.open('r') as f:
+        return BotConfig.model_validate(load(f))
